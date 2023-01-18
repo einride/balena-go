63d6f7f4841559fb8a9c80cd5195cda9f2f8e3d4
Cees Koolen <cakoolen@gmail.com>
2023-01-18 15:23:32 +0100

    Add some more missing fields

---
+++ devicetype.go
@@ -0,0 +1,12 @@
+package balena
+
+import "go.einride.tech/balena/odata"
+
+type DeviceType struct {
+	ID           uint64       `json:"id"`
+	Name         string       `json:"name"`
+	Slug         string       `json:"slug"`
+	UUID         string       `json:"uuid"`
+	IsPrivate    bool         `json:"is_private"`
+	Architecture odata.Object `json:"is_of__cpu_architecture"`
+}
