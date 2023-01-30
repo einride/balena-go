14563d1d0ec5d51f7d9e9616d49f1b909c6bd3b0
Cees Koolen <cakoolen@gmail.com>
2023-01-30 13:03:14 +0100

    feat: Add methods blink, update and device to supervisor v1 struct
    Added the following methods:
    - Blink: start a blink pattern on a LED for 15 s
    - Update: trigger a check for the target state of configurations
    - Device: return the current device state

--- supervisorv1.go
+++ supervisorv1.go
@@ -29,7 +29,7 @@ func (s *SupervisorV1Service) Reboot(ctx context.Context, force bool) error {
 		&request{Force: force},
 	)
 	if err != nil {
-		return fmt.Errorf("unable to create restart service request: %v", err)
+		return fmt.Errorf("unable to create restart service request: %w", err)
 	}
 	var resp struct {
 		Data  string `json:"Data"`
@@ -37,12 +37,94 @@ func (s *SupervisorV1Service) Reboot(ctx context.Context, force bool) error {
 	}
 	err = s.client.Do(req, &resp)
 	if err != nil {
-		return fmt.Errorf("unable to reboot device: %v", err)
+		return fmt.Errorf("unable to reboot device: %w", err)
 	}
 	if resp.Data != "OK" {
 		return fmt.Errorf("unable to reboot device: %v", resp.Error)
 	}
 	return nil
+}
+
+// Blink starts a blink pattern on a LED for 15 seconds, if your device has one.
+// https://docs.balena.io/reference/supervisor/supervisor-api/#post-v1blink
+func (s *SupervisorV1Service) Blink(ctx context.Context) error {
+	req, err := s.newRequest(
+		ctx,
+		http.MethodPost,
+		supervisorv1BasePath+"/blink",
+		nil,
+	)
+	if err != nil {
+		return fmt.Errorf("unable to create blink request: %w", err)
+	}
+
+	err = s.client.Do(req, nil)
+	if err != nil {
+		return fmt.Errorf("unable to start blink pattern: %w", err)
+	}
+
+	return nil
+}
+
+// Update triggers a check for the target state of configurations and app services.
+// https://docs.balena.io/reference/supervisor/supervisor-api/#post-v1update
+func (s *SupervisorV1Service) Update(ctx context.Context, force bool) error {
+	type request struct {
+		Force bool `json:"force"`
+	}
+	req, err := s.newRequest(
+		ctx,
+		http.MethodPost,
+		supervisorv1BasePath+"/update",
+		&request{Force: force},
+	)
+	if err != nil {
+		return fmt.Errorf("unable to create update request: %w", err)
+	}
+
+	err = s.client.Do(req, nil)
+	if err != nil {
+		return fmt.Errorf("unable to trigger update: %w", err)
+	}
+
+	return nil
+}
+
+type SupervisorV1DeviceResponse struct {
+	APIPort           uint   `json:"api_port,omitempty"`
+	Commit            string `json:"commit,omitempty"`
+	IPAddress         string `json:"ip_address,omitempty"`
+	MACAddress        string `json:"mac_address,omitempty"`
+	Status            string `json:"status,omitempty"`
+	DownloadProgress  uint   `json:"download_progress,omitempty"`
+	OSVersion         string `json:"os_version,omitempty"`
+	SupervisorVersion string `json:"supervisor_version,omitempty"`
+	UpdatePending     bool   `json:"update_pending,omitempty"`
+	UpdateDownloaded  bool   `json:"update_downloaded,omitempty"`
+	UpdateFailed      bool   `json:"update_failed,omitempty"`
+}
+
+// Device returns the current device state.
+// https://docs.balena.io/reference/supervisor/supervisor-api/#get-v1device
+func (s *SupervisorV1Service) Device(ctx context.Context) (*SupervisorV1DeviceResponse, error) {
+	req, err := s.newRequest(
+		ctx,
+		http.MethodGet,
+		supervisorv1BasePath+"/device",
+		nil,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create device request: %w", err)
+	}
+
+	var response SupervisorV1DeviceResponse
+
+	err = s.client.Do(req, &response)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get device state: %w", err)
+	}
+
+	return &response, nil
 }
 
 func (s *SupervisorV1Service) newRequest(
--- supervisorv1_test.go
+++ supervisorv1_test.go
@@ -2,6 +2,7 @@ package balena
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -93,4 +94,306 @@ func TestSupervisorV1Service_Reboot_Local(t *testing.T) {
 	err := client.Reboot(context.Background(), true)
 	// Then
 	assert.NilError(t, err)
+}
+
+func TestSupervisorV1Service_Blink_Cloud(t *testing.T) {
+	// Given
+	client, mux, cleanup := newFixture()
+	defer cleanup()
+	appID := int64(1514287)
+	deviceUUID := "00d859f123685e84772676f09465cc55"
+	mux.HandleFunc(
+		"/supervisor/v1/blink",
+		func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, http.MethodPost)
+			b, err := io.ReadAll(r.Body)
+			assert.NilError(t, err)
+			assert.Equal(
+				t,
+				`{"uuid":"00d859f123685e84772676f09465cc55","method":"POST"}`+"\n",
+				string(b),
+			)
+			w.WriteHeader(http.StatusOK)
+		},
+	)
+	// When
+	err := client.SupervisorV1(appID, deviceUUID).Blink(context.Background())
+	// Then
+	assert.NilError(t, err)
+}
+
+func TestSupervisorV1Service_Blink_CloudError(t *testing.T) {
+	// Given
+	client, mux, cleanup := newFixture()
+	defer cleanup()
+	appID := int64(1514287)
+	deviceUUID := "00d859f123685e84772676f09465cc55"
+	mux.HandleFunc(
+		"/supervisor/v1/blink",
+		func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, http.MethodPost)
+			b, err := io.ReadAll(r.Body)
+			assert.NilError(t, err)
+			assert.Equal(
+				t,
+				`{"uuid":"00d859f123685e84772676f09465cc55","method":"POST"}`+"\n",
+				string(b),
+			)
+			w.WriteHeader(http.StatusInternalServerError)
+		},
+	)
+	// When
+	err := client.SupervisorV1(appID, deviceUUID).Blink(context.Background())
+	// Then
+	assert.Assert(t, err != nil)
+
+	var errorResponse *ErrorResponse
+	assert.Check(t, errors.As(err, &errorResponse))
+	if errorResponse != nil {
+		assert.ErrorContains(t, errorResponse, "500")
+	}
+}
+
+func TestSupervisorV1Service_Blink_Local(t *testing.T) {
+	// Given
+	assert.NilError(t, os.Setenv("BALENA_SUPERVISOR_API_KEY", "test"))
+	assert.NilError(t, os.Setenv("BALENA_APP_ID", "1122334"))
+	assert.NilError(t, os.Setenv("BALENA_DEVICE_UUID", "11223344556677"))
+	client, mux := supervisorV1Fixture(t)
+	mux.HandleFunc(
+		"/v1/blink",
+		func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, http.MethodPost)
+			b, err := io.ReadAll(r.Body)
+			assert.NilError(t, err)
+			expected := "apikey=test"
+			if r.URL.RawQuery != expected {
+				http.Error(w, fmt.Sprintf("query = %s ; expected %s", r.URL.RawQuery, expected), http.StatusInternalServerError)
+				return
+			}
+			assert.Equal(t, "", string(b))
+			w.WriteHeader(http.StatusOK)
+		},
+	)
+	// When
+	err := client.Blink(context.Background())
+	// Then
+	assert.NilError(t, err)
+}
+
+func TestSupervisorV1Service_Update_Cloud(t *testing.T) {
+	// Given
+	client, mux, cleanup := newFixture()
+	defer cleanup()
+	appID := int64(1514287)
+	deviceUUID := "00d859f123685e84772676f09465cc55"
+	mux.HandleFunc(
+		"/supervisor/v1/update",
+		func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, http.MethodPost)
+			b, err := io.ReadAll(r.Body)
+			assert.NilError(t, err)
+			assert.Equal(
+				t,
+				`{"uuid":"00d859f123685e84772676f09465cc55","method":"POST","data":{"force":true}}`+"\n",
+				string(b),
+			)
+			w.WriteHeader(http.StatusNoContent)
+		},
+	)
+	// When
+	err := client.SupervisorV1(appID, deviceUUID).Update(context.Background(), true)
+	// Then
+	assert.NilError(t, err)
+}
+
+func TestSupervisorV1Service_Update_CloudError(t *testing.T) {
+	// Given
+	client, mux, cleanup := newFixture()
+	defer cleanup()
+	appID := int64(1514287)
+	deviceUUID := "00d859f123685e84772676f09465cc55"
+	mux.HandleFunc(
+		"/supervisor/v1/update",
+		func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, http.MethodPost)
+			b, err := io.ReadAll(r.Body)
+			assert.NilError(t, err)
+			assert.Equal(
+				t,
+				`{"uuid":"00d859f123685e84772676f09465cc55","method":"POST","data":{"force":true}}`+"\n",
+				string(b),
+			)
+			w.WriteHeader(http.StatusInternalServerError)
+		},
+	)
+	// When
+	err := client.SupervisorV1(appID, deviceUUID).Update(context.Background(), true)
+	// Then
+	assert.Assert(t, err != nil)
+
+	var errorResponse *ErrorResponse
+	assert.Check(t, errors.As(err, &errorResponse))
+	if errorResponse != nil {
+		assert.ErrorContains(t, errorResponse, "500")
+	}
+}
+
+func TestSupervisorV1Service_Update_Local(t *testing.T) {
+	// Given
+	assert.NilError(t, os.Setenv("BALENA_SUPERVISOR_API_KEY", "test"))
+	assert.NilError(t, os.Setenv("BALENA_APP_ID", "1122334"))
+	assert.NilError(t, os.Setenv("BALENA_DEVICE_UUID", "11223344556677"))
+	client, mux := supervisorV1Fixture(t)
+	mux.HandleFunc(
+		"/v1/update",
+		func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, http.MethodPost)
+			b, err := io.ReadAll(r.Body)
+			assert.NilError(t, err)
+			expected := "apikey=test"
+			if r.URL.RawQuery != expected {
+				http.Error(w, fmt.Sprintf("query = %s ; expected %s", r.URL.RawQuery, expected), http.StatusInternalServerError)
+				return
+			}
+			assert.Equal(
+				t,
+				`{"force":true}`+"\n",
+				string(b),
+			)
+			w.WriteHeader(http.StatusNoContent)
+		},
+	)
+	// When
+	err := client.Update(context.Background(), true)
+	// Then
+	assert.NilError(t, err)
+}
+
+const (
+	supervisorV1DeviceResponse = `{
+		"api_port":48484,
+		"ip_address":"192.168.0.114 10.42.0.3",
+		"commit":"414e65cd378a69a96f403b75f14b40b55856f860",
+		"status":"Downloading",
+		"download_progress":84,
+		"os_version":"Resin OS 1.0.4 (fido)",
+		"supervisor_version":"1.6.0",
+		"update_pending":true,
+		"update_downloaded":false,
+		"update_failed":false
+	}`
+)
+
+func TestSupervisorV1Service_Device_Cloud(t *testing.T) {
+	// Given
+	client, mux, cleanup := newFixture()
+	defer cleanup()
+	appID := int64(1514287)
+	deviceUUID := "00d859f123685e84772676f09465cc55"
+	mux.HandleFunc(
+		"/supervisor/v1/device",
+		func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, http.MethodPost)
+			b, err := io.ReadAll(r.Body)
+			assert.NilError(t, err)
+			assert.Equal(
+				t,
+				`{"uuid":"00d859f123685e84772676f09465cc55","method":"GET"}`+"\n",
+				string(b),
+			)
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, supervisorV1DeviceResponse)
+		},
+	)
+	// When
+	response, err := client.SupervisorV1(appID, deviceUUID).Device(context.Background())
+	// Then
+	assert.NilError(t, err)
+	assert.DeepEqual(t, response, &SupervisorV1DeviceResponse{
+		APIPort:           48484,
+		Commit:            "414e65cd378a69a96f403b75f14b40b55856f860",
+		IPAddress:         "192.168.0.114 10.42.0.3",
+		Status:            "Downloading",
+		DownloadProgress:  84,
+		OSVersion:         "Resin OS 1.0.4 (fido)",
+		SupervisorVersion: "1.6.0",
+		UpdatePending:     true,
+		UpdateDownloaded:  false,
+		UpdateFailed:      false,
+	})
+}
+
+func TestSupervisorV1Service_Device_CloudError(t *testing.T) {
+	// Given
+	client, mux, cleanup := newFixture()
+	defer cleanup()
+	appID := int64(1514287)
+	deviceUUID := "00d859f123685e84772676f09465cc55"
+	mux.HandleFunc(
+		"/supervisor/v1/device",
+		func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, http.MethodPost)
+			b, err := io.ReadAll(r.Body)
+			assert.NilError(t, err)
+			assert.Equal(
+				t,
+				`{"uuid":"00d859f123685e84772676f09465cc55","method":"GET"}`+"\n",
+				string(b),
+			)
+			w.WriteHeader(http.StatusInternalServerError)
+		},
+	)
+	// When
+	response, err := client.SupervisorV1(appID, deviceUUID).Device(context.Background())
+	// Then
+	assert.Assert(t, err != nil)
+
+	var errorResponse *ErrorResponse
+	assert.Check(t, errors.As(err, &errorResponse))
+	if errorResponse != nil {
+		assert.ErrorContains(t, errorResponse, "500")
+	}
+
+	assert.Assert(t, response == nil)
+}
+
+func TestSupervisorV1Service_Device_Local(t *testing.T) {
+	// Given
+	assert.NilError(t, os.Setenv("BALENA_SUPERVISOR_API_KEY", "test"))
+	assert.NilError(t, os.Setenv("BALENA_APP_ID", "1122334"))
+	assert.NilError(t, os.Setenv("BALENA_DEVICE_UUID", "11223344556677"))
+	client, mux := supervisorV1Fixture(t)
+	mux.HandleFunc(
+		"/v1/device",
+		func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, http.MethodGet)
+			b, err := io.ReadAll(r.Body)
+			assert.NilError(t, err)
+			expected := "apikey=test"
+			if r.URL.RawQuery != expected {
+				http.Error(w, fmt.Sprintf("query = %s ; expected %s", r.URL.RawQuery, expected), http.StatusInternalServerError)
+				return
+			}
+			assert.Equal(t, "", string(b))
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, supervisorV1DeviceResponse)
+		},
+	)
+	// When
+	response, err := client.Device(context.Background())
+	// Then
+	assert.NilError(t, err)
+	assert.DeepEqual(t, response, &SupervisorV1DeviceResponse{
+		APIPort:           48484,
+		Commit:            "414e65cd378a69a96f403b75f14b40b55856f860",
+		IPAddress:         "192.168.0.114 10.42.0.3",
+		Status:            "Downloading",
+		DownloadProgress:  84,
+		OSVersion:         "Resin OS 1.0.4 (fido)",
+		SupervisorVersion: "1.6.0",
+		UpdatePending:     true,
+		UpdateDownloaded:  false,
+		UpdateFailed:      false,
+	})
 }
